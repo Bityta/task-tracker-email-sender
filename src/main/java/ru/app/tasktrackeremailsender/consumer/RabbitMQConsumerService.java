ac02b7f59c78b8d9bae3f01e055a7e8c52dd6ac7
Bityta <bituckihs@gmail.com>
2024-02-21 20:08:35 +0300

    add annotation @Service

--- src/main/java/ru/app/tasktrackeremailsender/consumer/RabbitMQConsumerService.java
+++ src/main/java/ru/app/tasktrackeremailsender/consumer/RabbitMQConsumerService.java
@@ -3,7 +3,9 @@ package ru.app.tasktrackeremailsender.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RabbitMQConsumerService {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitMQConsumerService.class);
