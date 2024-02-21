d0944495c8091afea48c1c17e17d7fde582380de
Bityta <bituckihs@gmail.com>
2024-02-21 19:27:37 +0300

    create RabbitMQService

---
+++ src/main/java/ru/app/tasktrackeremailsender/publisher/RabbitMQPublisherService.java
@@ -0,0 +1,30 @@
+package ru.app.tasktrackeremailsender.publisher;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQPublisherService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQPublisherService.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.key.name}")
+    private String key;
+
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendGreetingsMessage(String email){
+        logger.info(String.format("Message sent -> %s", email));
+        this.rabbitTemplate.convertAndSend(exchange, key, email);
+    }
+
+}
