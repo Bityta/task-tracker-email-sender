1923522d5a433655944d238461856659899a479a
Bityta <bituckihs@gmail.com>
2024-02-24 02:52:12 +0300

    adding an annotation @EnableRabbit

--- src/main/java/ru/app/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
+++ src/main/java/ru/app/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
@@ -1,9 +1,11 @@
 package ru.app.tasktrackeremailsender;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class TaskTrackerEmailSenderApplication {
 
     public static void main(String[] args) {
