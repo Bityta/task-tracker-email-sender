75f4e178286deebc53c13bf8358fce84a2aca80a
Bityta <bituckihs@gmail.com>
2024-02-24 15:33:02 +0300

    dependency mailService

--- src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/service/publisher/RabbitMQPublisherService.java
+++ src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/service/publisher/RabbitMQPublisherService.java
@@ -1,4 +1,4 @@
-package ru.app.tasktrackeremailsender.publisher;
+package ru.app.tasktrackeremailsender.rabbitMQ.service.publisher;
 
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.app.tasktrackeremailsender.email.service.EmailService;
 
 @Service
 @RequiredArgsConstructor
@@ -13,13 +14,18 @@ public class RabbitMQPublisherService {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitMQPublisherService.class);
     private final RabbitTemplate rabbitTemplate;
+    private final EmailService emailService;
     @Value("${rabbitmq.exchange}")
     private String exchange;
     @Value("${rabbitmq.key}")
     private String key;
+    private static final String GREETINGS_HEADER = "Greetings from Task-tracker-email-sender";
+    private static final String GREETINGS_BODY = "Thank you for registering";
+
 
     public void sendGreetingsMessage(String email) {
         logger.info(String.format("Message sent -> %s", email));
+        this.emailService.sendMessage(email, GREETINGS_HEADER, GREETINGS_BODY);
         this.rabbitTemplate.convertAndSend(exchange, key, email);
     }
 
