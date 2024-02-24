506259c83074dfae0b6cdc7346c41699d8183076
Bityta <bituckihs@gmail.com>
2024-02-24 20:20:34 +0300

    add logger

--- src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/service/publisher/RabbitMQPublisherService.java
+++ src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/service/publisher/RabbitMQPublisherService.java
@@ -1,8 +1,6 @@
 package ru.app.tasktrackeremailsender.rabbitMQ.service.publisher;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,7 +10,6 @@ import ru.app.tasktrackeremailsender.email.service.EmailService;
 @RequiredArgsConstructor
 public class RabbitMQPublisherService {
 
-    private static final Logger logger = LoggerFactory.getLogger(RabbitMQPublisherService.class);
     private static final String GREETINGS_HEADER = "Greetings from Task-tracker-email-sender";
     private static final String GREETINGS_BODY = "Thank you for registering";
     private final RabbitTemplate rabbitTemplate;
@@ -23,7 +20,6 @@ public class RabbitMQPublisherService {
     private String key;
 
     public void sendGreetingsMessage(String email) {
-        logger.info(String.format("Message sent -> %s", email));
         this.emailService.sendMessage(email, GREETINGS_HEADER, GREETINGS_BODY);
         this.rabbitTemplate.convertAndSend(exchange, key, email);
     }
