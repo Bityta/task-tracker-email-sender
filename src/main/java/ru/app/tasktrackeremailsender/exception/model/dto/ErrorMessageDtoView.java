a911308fa589c93f19889c4a4af7b09e6f3f1e3c
Bityta <bituckihs@gmail.com>
2024-02-26 01:37:37 +0300

    code refactoring

--- src/main/java/ru/app/tasktrackeremailsender/exception/model/dto/ErrorMessageDtoView.java
+++ src/main/java/ru/app/tasktrackeremailsender/exception/model/dto/ErrorMessageDtoView.java
@@ -3,15 +3,15 @@ package ru.app.tasktrackeremailsender.exception.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -21,14 +21,15 @@ public class ErrorMessageDtoView {
     private String fieldName;
     private String error;
     private String path;
-
+    private String timestamp;
 
     public Map<String, String> getError() {
-
         Map<String, String> errors = new HashMap<>();
-        Date date = new Date(System.currentTimeMillis());
 
-        errors.put("timestamp", String.valueOf(date.toString()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedTimestamp = dateFormat.format(new Date());
+
+        errors.put("timestamp", formattedTimestamp);
         errors.put("status", String.valueOf(this.status));
         errors.put("error", this.error);
         errors.put("path", this.path);
