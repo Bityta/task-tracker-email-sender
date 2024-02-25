f619bc2c1b65c34bb80704d168c47c7f9673b19e
Bityta <bituckihs@gmail.com>
2024-02-26 01:37:06 +0300

    @data -> @setter @getter

--- src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/model/dto/email/EmailDto.java
+++ src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/model/dto/email/EmailDto.java
@@ -1,17 +1,15 @@
 package ru.app.tasktrackeremailsender.rabbitMQ.model.dto.email;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Builder
+@Getter
+@Setter
 @Schema(name = "Email", description = "Information about email")
-@Data
 public class EmailDto {
-
     private String email;
 }
