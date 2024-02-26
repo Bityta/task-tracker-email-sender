5712eee096cb015d15eac39622403a0db7087d3c
Bityta <bituckihs@gmail.com>
2024-02-26 17:55:35 +0300

    add jsonMessageConverter

--- src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/config/RabbitMQConfig.java
+++ src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/config/RabbitMQConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,23 +17,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${rabbitmq.queue}")
-    private String queueName;
+    @Value("${rabbitmq.queue.greetings}")
+    private String queueNameGreetings;
+
+    @Value("${rabbitmq.queue.analytics}")
+    private String queueNameAnalytics;
 
     @Value("${rabbitmq.exchange}")
     private String exchangeName;
 
-    @Value("${rabbitmq.key}")
-    private String keyName;
+    @Value("${rabbitmq.key.greetings}")
+    private String routingKeyGreetings;
+
+    @Value("${rabbitmq.key.analytics}")
+    private String routingKeyAnalytics;
 
     /**
-     * Bean definition for creating the message queue.
+     * Bean definition for creating the message queue for greetings.
      *
      * @return A Queue instance.
      */
     @Bean
-    public Queue queue() {
-        return new Queue(this.queueName);
+    public Queue queueGreetings() {
+        return new Queue(this.queueNameGreetings);
+    }
+
+    /**
+     * Bean definition for creating the message queue for analytics.
+     *
+     * @return A Queue instance.
+     */
+    @Bean
+    public Queue queueAnalytics() {
+        return new Queue(this.queueNameAnalytics);
     }
 
     /**
@@ -44,14 +63,49 @@ public class RabbitMQConfig {
     }
 
     /**
-     * Bean definition for creating the binding between queue and exchange.
+     * Bean definition for creating the binding between greetings queue and exchange.
      *
+     * @param queueGreetings The greetings queue.
+     * @param exchange       The topic exchange.
      * @return A Binding instance.
      */
     @Bean
-    public Binding Binding() {
-        return BindingBuilder.bind(this.queue())
-                .to(this.exchange())
-                .with(this.keyName);
+    public Binding bindingGreetings(Queue queueGreetings, TopicExchange exchange) {
+        return BindingBuilder.bind(queueGreetings).to(exchange).with(this.routingKeyGreetings);
+    }
+
+    /**
+     * Bean definition for creating the binding between analytics queue and exchange.
+     *
+     * @param queueAnalytics The analytics queue.
+     * @param exchange       The topic exchange.
+     * @return A Binding instance.
+     */
+    @Bean
+    public Binding bindingAnalytics(Queue queueAnalytics, TopicExchange exchange) {
+        return BindingBuilder.bind(queueAnalytics).to(exchange).with(this.routingKeyAnalytics);
+    }
+
+    /**
+     * Bean definition for creating the Jackson2JsonMessageConverter.
+     *
+     * @return A Jackson2JsonMessageConverter instance.
+     */
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * Bean definition for creating the RabbitTemplate.
+     *
+     * @param connectionFactory The connection factory.
+     * @return A RabbitTemplate instance.
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(this.jsonMessageConverter());
+        return rabbitTemplate;
     }
 }
