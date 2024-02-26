7777b66ccf7fe362616ea743547ef169665e0610
Bityta <bituckihs@gmail.com>
2024-02-26 18:15:12 +0300

    add valid

--- src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/exceptionHandler/MessageExceptionHandler.java
+++ src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/exceptionHandler/MessageExceptionHandler.java
@@ -5,13 +5,17 @@ import jakarta.mail.SendFailedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.app.tasktrackeremailsender.exception.model.dto.ErrorMessageDtoView;
 import ru.app.tasktrackeremailsender.rabbitMQ.controller.MessageController;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice(assignableTypes = MessageController.class)
 @Hidden()
@@ -36,5 +40,34 @@ public class MessageExceptionHandler {
                 .build();
 
         return errors.getError();
+    }
+
+    /**
+     * Handles MethodArgumentNotValidException and returns a response with HTTP status code 400 Bad Request.
+     *
+     * @param ex The MethodArgumentNotValidException instance.
+     * @return A map containing error information.
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        LOGGER.error("User data validation error. {}", ex.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorMessageDtoView error = ErrorMessageDtoView.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .error(errors.keySet().stream()
+                        .map(errors::get)
+                        .collect(Collectors.joining(". ")))
+                .path("/tasks")
+                .build();
+
+        return error.getError();
     }
 }
