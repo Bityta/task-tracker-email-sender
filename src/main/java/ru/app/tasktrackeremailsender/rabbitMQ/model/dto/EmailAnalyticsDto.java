37ea12cd0c29b315e085d00fe694961376fc7f9b
Bityta <bituckihs@gmail.com>
2024-02-26 18:30:58 +0300

    added documentation

--- src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/model/dto/EmailAnalyticsDto.java
+++ src/main/java/ru/app/tasktrackeremailsender/rabbitMQ/model/dto/EmailAnalyticsDto.java
@@ -17,17 +17,26 @@ import lombok.*;
 @Schema(name = "EmailAnalyticsDto", description = "Information about email analytics")
 public class EmailAnalyticsDto {
 
+    /**
+     * The email address.
+     */
     @Schema(description = "Email address")
     @Size(min = 5, max = 50, message = "Email must contain from 5 to 50 characters")
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
 
+    /**
+     * The header of the email.
+     */
     @Schema(description = "Header of the email")
     @Size(min = 5, max = 50, message = "Header must contain from 5 to 50 characters")
     @NotBlank(message = "Header cannot be empty")
     private String header;
 
+    /**
+     * The body of the email.
+     */
     @Schema(description = "Body of the email")
     @Size(min = 5, max = 4096, message = "Body must contain from 5 to 4096 characters")
     @NotBlank(message = "Body cannot be empty")
