6061b7efa2a7f5b7e85b57aab5c940f57135901d
Bityta <bituckihs@gmail.com>
2024-02-26 03:13:15 +0300

    added documentation

--- src/main/java/ru/app/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
+++ src/main/java/ru/app/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
@@ -4,13 +4,19 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class to start the Task Tracker Email Sender application.
+ */
 @SpringBootApplication
 @EnableRabbit
 public class TaskTrackerEmailSenderApplication {
 
+    /**
+     * Main method to start the Task Tracker Email Sender application.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
     }
-
-
 }
